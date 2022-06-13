211db5edf2557ad12a843f38fa8a7eee23e981e1
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2022-06-13 17:14:41 +0200

    feat: Trying to run Epsilon from Java

---
+++ src/cityMM/ExcecutionEnginne.java
@@ -0,0 +1,14 @@
+package cityMM;
+
+public class ExcecutionEnginne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Trying to automate the process");
+//		EolModule module = new EolModule();
+//		module.parse(new File("program.eol"));
+//		module.execute();
+
+	}
+
+}
